f09abfc842b2c7522afaa3e5ab05440b2d6d51d5
Mikhail Surin <surinmike@gmail.com>
2018-11-04 20:02:36 +0300

    add acls

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import requests
 import subprocess
 import logging
+from base import Session, Role, User, drop_all
 
 updater = Updater(os.environ['TELEGRAM_TOKEN'])
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -49,13 +50,12 @@ def replyerrors(func):
 def guard(predicate, message=None):
     def decorator(func):
         def handler(bot, update):
-            chat = update.message.chat
-            if predicate(chat):
+            if predicate(update):
                 func(bot, update)
             else:
                 if message is not None:
                     update.message.reply_text(message, quote=True)
-                log.info('denied access for %s', chat)
+                log.info('denied access for %s', update.message.chat)
         return handler
     return decorator
 
@@ -75,15 +75,27 @@ def retry(cnt):
     return decorator
 
 
+def check_role(role_name):
+    def check(update):
+        user = Session().query(User).filter(User.id==update.message.from_user.id).one()
+        for role in user.roles:
+            if role.name == role_name:
+                return True
+        return False
+    return guard(check, "you are not {}".format(role_name))
+
+
 def owner(user):
-    def pred(chat):
-        return chat.type == 'private' and chat.username == user
+    def pred(update):
+        return update.message.chat.type == 'private' and \
+               update.message.from_user.username == user
     return guard(pred, "only {} has access to this handler".format(user))
 
 
 def owners(users):
-    def pred(chat):
-        return chat.type == 'private' and chat.username in users
+    def pred(update):
+        return update.message.chat.type == 'private' and \
+               update.message.from_user.username in users
     return guard(pred, "only {} have access to this handler".format(", ".join(users)))
 
 
@@ -103,7 +115,7 @@ def get_call_result(command):
 
 
 @command('shell')
-@owner('ssmike')
+@check_role('admin')
 @replyerrors
 def shell(bot, update):
     command = update.message.text.split(' ', 1)[1]
@@ -111,7 +123,7 @@ def shell(bot, update):
 
 
 @command('free')
-@owner('ssmike')
+@check_role('user')
 def free(bot, update):
     update.message.reply_text(get_call_result('free -m'), quote=True)
 
@@ -129,7 +141,7 @@ def deploy(bot, update):
 
 
 @command('yasm')
-@owner('ssmike')
+@check_role('user')
 @replyerrors
 @retry(3)
 def snapshot(bot, update):
@@ -154,7 +166,7 @@ def snapshot(bot, update):
 
 
 @command('log')
-@owner('ssmike')
+@check_role('admin')
 def toggle_logging(bot, update):
     global enabled_logging
     chat_id = update.message.chat.id
@@ -164,6 +176,95 @@ def toggle_logging(bot, update):
     elif action == 'disable':
         if chat_id in enabled_logging:
             enabled_logging.remove(chat_id)
+
+
+@command('acl_init')
+@owner('ssmike')
+@replyerrors
+def clr_acl(bot, update):
+    admin_role = Role(name='admin')
+    user_role = Role(name='user')
+    user = User(name='ssmike', id=update.message.from_user.id, roles=[admin_role, user_role])
+
+    session = Session()
+    session.add_all([user_role, admin_role, user])
+    session.commit()
+
+
+@command('acl_clear')
+@owner('ssmike')
+@replyerrors
+def ctl_clr(bot, update):
+    drop_all()
+
+
+@command('add_role')
+@check_role('admin')
+def add_role(bot, update):
+    name = update.message.text.split(' ')[1]
+
+    session = Session()
+    session.add(Role(name=name))
+    session.commit()
+
+
+@command('start')
+def add_user(bot, update):
+    try:
+        user = update.message.from_user
+        session = Session()
+        session.add(User(name=user.username, id=user.id))
+        session.commit()
+    except Exception:
+        return
+
+
+def parse_role_users(text):
+    names, role_names = [], []
+    for token in text.split(' ')[1:]:
+        if token.startswith('@'):
+            names.append(token[1:])
+        else:
+            role_names.append(token)
+    return names, role_names
+
+
+@command('acl_add')
+@check_role('admin')
+def add_roles(bot, update):
+    session = Session()
+    text = update.message.text
+    names, role_names = parse_role_users(text)
+    for user, role in session.query(User, Role) \
+                             .filter(Role.name.in_(role_names)) \
+                             .filter(User.name.in_(names)).all():
+        user.roles.append(role)
+    session.commit()
+
+
+@command('acl_rm')
+@check_role('admin')
+def del_roles(bot, update):
+    session = Session()
+    text = update.message.text
+    names, role_names = parse_role_users(text)
+    for user, role in session.query(User, Role) \
+                             .filter(Role.name.in_(role_names)) \
+                             .filter(User.name.in_(names)).all():
+        user.roles.remove(role)
+    session.commit()
+
+
+@command('acl_list')
+def list_users(bot, update):
+    name = update.message.text.split(' ')[1]
+    session = Session()
+    role = session.query(Role).filter(Role.name == name).one()
+    result = []
+    for user in role.users:
+        result.append(user.name)
+    if len(result) != 0:
+        update.message.reply_text("\n".join(result), quote=True)
 
 
 updater.start_polling()
