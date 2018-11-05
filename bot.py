2b116063c43bd279edf028a47871b33ff4476edd
Mikhail Surin <surinmike@gmail.com>
2018-11-05 21:14:09 +0300

    explicitly close sessions

--- bot.py
+++ bot.py
@@ -7,14 +7,34 @@ import shutil
 import uuid
 from base import Bookmark, Watch, Role, User, Session, drop
 from screenshot import make_screenshot
+from contextlib import contextmanager
 
 updater = Updater(os.environ['TELEGRAM_TOKEN'], workers=8)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
 
-def broadcast_chats(func, *filters):
+@contextmanager
+def make_session():
     session = Session()
+    try:
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def with_session(func):
+    def result(*args, **kwargs):
+        with make_session() as session:
+            return func(session, *args, **kwargs)
+    return result
+
+
+@with_session
+def broadcast_chats(session, func, *filters):
     ids = set()
     for watch in session.query(Watch).filter(Watch.filter.in_(filters)).all():
         if watch.chat_id not in ids:
@@ -90,8 +110,9 @@ def retry(cnt):
 
 
 def check_role(role_name):
-    def check(update):
-        return Session().query(User)\
+    @with_session
+    def check(session, update):
+        return session.query(User)\
                 .filter(User.id == update.message.from_user.id)\
                 .join(User.roles)\
                 .filter(Role.name == role_name)\
@@ -155,8 +176,8 @@ def deploy(bot, update):
 
 @command('watch')
 @check_role('watcher')
-def toggle_logging(bot, update):
-    session = Session()
+@with_session
+def toggle_logging(session, bot, update):
     chat_id = update.message.chat.id
     tokens = update.message.text.split(' ', 2)
     action = tokens[1]
@@ -169,30 +190,26 @@ def toggle_logging(bot, update):
                 .filter(Watch.chat_id == chat_id)\
                 .filter(Watch.filter.in_(filters_))\
                 .delete(synchronize_session='fetch')
-    session.commit()
 
 
 @command('new_role')
 @check_role('admin')
-def add_role(bot, update):
+@with_session
+def add_role(session, bot, update):
     role = update.message.text.split(' ', 1)[1]
-    session = Session()
     session.add(Role(name=role))
-    session.commit()
 
 
 @command('acl_init')
 @owner('ssmike')
 @replyerrors
-def clr_acl(bot, update):
+@with_session
+def clr_acl(session, bot, update):
     watch_role = Role(name='watcher')
     admin_role = Role(name='admin')
     user_role = Role(name='user')
     user = User(name='ssmike', id=update.message.from_user.id, roles=[admin_role, watch_role, user_role])
-
-    session = Session()
     session.add_all([user_role, admin_role, watch_role, user])
-    session.commit()
 
 
 @command('drop')
@@ -205,15 +222,11 @@ def drop_tables(bot, update):
 
 
 @command('start')
-def add_user(bot, update):
-    try:
-        user = update.message.from_user
-        session = Session()
-        session.add(User(name=user.username, id=user.id))
-        session.add(Watch(filter='announces', chat_id=update.message.chat))
-        session.commit()
-    except Exception:
-        return
+@with_session
+def add_user(session, bot, update):
+    user = update.message.from_user
+    session.add(User(name=user.username, id=user.id))
+    session.add(Watch(filter='announces', chat_id=update.message.chat))
 
 
 def parse_role_users(text):
@@ -229,36 +242,34 @@ def parse_role_users(text):
 @command('acl_add')
 @check_role('admin')
 @replyerrors
-def add_roles(bot, update):
-    session = Session()
+@with_session
+def add_roles(session, bot, update):
     text = update.message.text
     names, role_names = parse_role_users(text)
     for user, role in session.query(User, Role) \
                              .filter(Role.name.in_(role_names)) \
                              .filter(User.name.in_(names)).all():
         user.roles.append(role)
-    session.commit()
 
 
 @command('acl_rm')
 @check_role('admin')
 @replyerrors
-def del_roles(bot, update):
-    session = Session()
+@with_session
+def del_roles(session, bot, update):
     text = update.message.text
     names, role_names = parse_role_users(text)
     for user, role in session.query(User, Role) \
                              .filter(Role.name.in_(role_names)) \
                              .filter(User.name.in_(names)).all():
         user.roles.remove(role)
-    session.commit()
 
 
 @command('acl_list')
 @check_role('admin')
-def list_users(bot, update):
+@with_session
+def list_users(session, bot, update):
     name = update.message.text.split(' ')[1]
-    session = Session()
     role = session.query(Role).filter(Role.name == name).one()
     result = []
     for user in role.users:
@@ -282,24 +293,22 @@ def download(url):
 
 @command('bookmark')
 @check_role('user')
-def add_url(bot, update):
+@with_session
+def add_url(session, bot, update):
     alias, url = update.message.text.split(' ', 2)[1:]
-    session = Session()
     user = session.query(User).filter(User.id == update.message.from_user.id).one()
     session.add(Bookmark(shortname=alias, url=url, user=user))
-    session.commit()
 
 
 @command('bdelete')
 @check_role('user')
-def rm_url(bot, update):
+@with_session
+def rm_url(session, bot, update):
     urls = update.message.text.split(' ')[1:]
-    session = Session()
     session.query(Bookmark)\
            .filter(Bookmark.user_id == update.message.from_user.id)\
            .filter(Bookmark.shortname.in_(urls))\
            .delete(synchronize_session='fetch')
-    session.commit()
 
 
 @command('fetch')
@@ -309,20 +318,23 @@ def rm_url(bot, update):
 def send_doc(bot, update):
     args = update.message.text.split(' ', 3)
     user_id = update.message.from_user.id
-    session = Session()
-    user = session.query(User).filter(User.id == update.message.from_user.id).one()
-    if len(args) < 2:
-        resp = "\n".join("%s: %s" % (b.shortname, b.url) for b in user.bookmarks)
-        update.message.reply_text(resp, quote=True)
-    else:
-        explicit_sleep = None
-        if len(args) >= 3:
-            explicit_sleep = int(args[1])
-        url = session.query(Bookmark)\
-                     .filter(Bookmark.user_id == user_id)\
-                     .filter(Bookmark.shortname == args[-1])\
-                     .one().url
-        fname = gen_fname()
+    fname = None
+    with make_session() as session:
+        user = session.query(User).filter(User.id == update.message.from_user.id).one()
+        if len(args) < 2:
+            resp = "\n".join("%s: %s" % (b.shortname, b.url) for b in user.bookmarks)
+            update.message.reply_text(resp, quote=True)
+        else:
+            explicit_sleep = None
+            if len(args) >= 3:
+                explicit_sleep = int(args[1])
+            url = session.query(Bookmark)\
+                         .filter(Bookmark.user_id == user_id)\
+                         .filter(Bookmark.shortname == args[-1])\
+                         .one().url
+            fname = gen_fname()
+
+    if fname:
         make_screenshot(url, fname, explicit_sleep)
         with open(fname, 'rb') as fin:
             update.message.reply_document(fin, quote=True)
