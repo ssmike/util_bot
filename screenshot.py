2357016f0c56337f1364a9d7a82d2aecf4315e0d
Mikhail Surin <surinmike@gmail.com>
2018-11-05 04:05:54 +0300

    fixed size

--- screenshot.py
+++ screenshot.py
@@ -3,9 +3,14 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument('--headless')
+options.add_argument('--window-size=1366,768')
 
 
 def make_screenshot(url, fname):
     browser = Chrome(chrome_options=options)
     browser.get(url)
     browser.save_screenshot(fname)
+
+
+if __name__ == '__main__':
+    make_screenshot('https://github.com', 'screenshot.png')
