b1de31c079389e8a429626bb2f503946dac41c5c
Mikhail Surin <surinmike@gmail.com>
2020-04-29 15:53:48 +0300

    fix chatid

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ def drop_tables(bot, update):
 def add_user(session, bot, update):
     user = update.message.from_user
     session.add(User(name=user.username, id=user.id))
-    session.add(Watch(filter='announces', chat_id=update.message.chat))
+    session.add(Watch(filter='announces', chat_id=update.message.chat.id))
 
 
 def gen_fname():
