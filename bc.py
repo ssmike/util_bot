d1adb46f2ee0b55ae4d8df0ab825b304750fe8b6
Mikhail Surin <surinmike@gmail.com>
2021-05-19 21:33:35 +0300

    fix pep

--- bc.py
+++ bc.py
@@ -10,6 +10,7 @@ operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
              ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
              ast.USub: op.neg, ast.Mod: op.mod}
 
+
 def eval_expr(expr):
     """
     >>> eval_expr('2^6')
@@ -21,12 +22,13 @@ def eval_expr(expr):
     """
     return eval_(ast.parse(expr, mode='eval').body)
 
+
 def eval_(node):
-    if isinstance(node, ast.Num): # <number>
+    if isinstance(node, ast.Num):  # <number>
         return node.n
-    elif isinstance(node, ast.BinOp): # <left> <operator> <right>
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
         return operators[type(node.op)](eval_(node.left), eval_(node.right))
-    elif isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
         return operators[type(node.op)](eval_(node.operand))
     else:
         raise TypeError(node)
